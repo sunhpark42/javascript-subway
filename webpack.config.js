823f2c8f339942a8027572813fc0ec4378a6a226
sunhpark42 <sunhpark42@gmail.com>
2021-10-24 01:28:49 +0900

    feat: redirect path 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -36,5 +37,8 @@ module.exports = {
     new webpack.ProgressPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({ filename: 'index.html', template: './index.html' }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './public', to: './public' }],
+    }),
   ],
 };
